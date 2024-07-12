2fb25d6e9c3afb700948673060e45076f889d02f
Nobert-Muma <mumanobert17@gmail.com>
2024-07-12 21:18:22 +0300

    DOM Challenge

--- js/challenge.js
+++ js/challenge.js
@@ -1,6 +1,12 @@
 let counter=document.getElementById("counter");
 let addi=parseInt(counter.textContent);
+const pause=document.getElementById("pause");
+const disable=document.querySelectorAll(".dis");
+const form=document.getElementsByTagName("form")[0];
+const inputValue=document.getElementById("comment-input");
+const submmit=document.getElementById("submit");
 let likeCount=0;
+let isPaused=false;
 function countt(){
     //let addi=parseInt(counter.textContent);
     addi+=1;
@@ -8,7 +14,7 @@ function countt(){
 }
 
 document.addEventListener("DOMContentLoaded", ()=>{
-    setInterval(countt,1000)
+    const intervall=setInterval(countt,1000);
     const minus=document.getElementById("minus");
     minus.addEventListener("click", ()=>{
         addi=addi-1;
@@ -21,7 +27,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
     });
     const heart=document.getElementById("heart");
     heart.addEventListener("click", ()=>{
-        likeCount++;
+        likeCount+=1;
         const likes=document.getElementsByClassName("likes")[0];
         const li=document.createElement("li");
         if(likeCount===1){
@@ -31,6 +37,27 @@ document.addEventListener("DOMContentLoaded", ()=>{
         }
         likes.appendChild(li);
         //console.log(likes)
+    });
+    // const pause=document.getElementById("pause");
+    pause.addEventListener("click", ()=>{
+        
+        isPaused=true;
+        pause.textContent="Resume";
+        // const disable=document.querySelectorAll(".dis");
+        disable.forEach(button=>button.setAttribute("disabled", ""));
+        clearInterval(intervall);
+        intervall=null;
+    });
+    submmit.addEventListener("click",(e)=>{
+        e.preventDefault();
+        let keyedValue=inputValue.value;
+        const h3=document.getElementsByTagName("h3")[0];
+        h3.append(keyedValue);
+        h3.append(document.createElement("br"))
+        inputValue.value="";
+
     })
+   
+
     
 })
